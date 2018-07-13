882a65bda124f4573e4d04c4890f60b6097b386d
Ruben Steeb <ruben.steeb@gmail.com>
2018-07-13 11:10:07 +0200

    [DEBUG] Degubg Language Service in Record List

--- Classes/RecordList/TestRecordList.php
+++ Classes/RecordList/TestRecordList.php
@@ -7,7 +7,10 @@ class TestRecordList extends DatabaseRecordList {
 
 
     public function getTable($tableName, $id, $fields ='') {
-        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($GLOBALS, 'GLOBALS');
+        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($this->getLanguageService(), 'LanguageService');
         return 'Output Of the Record List';
     }
+
+
+
 }
