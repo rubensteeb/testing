e241b993db98180a302e22e17631c36b872d187a
Ruben Steeb <ruben.steeb@gmail.com>
2018-07-13 08:54:27 +0200

    [TAKS] Add Controller and Test Backend Template

---
+++ Classes/Controller/MainController.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+use TYPO3\CMS\Extbase\Mvc\View\ViewInterface;
+use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
+use TYPO3\CMS\Backend\Template\Components\ButtonBar;
+use TYPO3\CMS\Backend\View\BackendTemplateView;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+
+class MainController extends ActionController {
+    /**
+     * Default View Container
+     * 
+     * @var string
+     */
+    protected $defaultViewObjectName = BackendTemplateView::class;
+
+    /**
+     * BackendTemplateContainer
+     * 
+     * @var BackendTemplateView
+     */
+    protected $view;
+
+    /**
+     * set up the doc header properly
+     * 
+     * @param ViewInterafce $view
+     * @return void
+     */
+    protected function initializeView(ViewInterface $view) {
+        /** @var BackendTemplateView */
+        parent::initalizeView($view);
+    }
+
+    public function indexAction() {
+        
+    }
+}
