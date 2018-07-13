ac4bb3063e8e11f2e9a130572d8ca973d92f60f5
Ruben Steeb <ruben.steeb@gmail.com>
2018-07-13 11:30:36 +0200

    [DEBUG] Degubg Language Service in Record List

--- Classes/RecordList/TestRecordList.php
+++ Classes/RecordList/TestRecordList.php
@@ -2,17 +2,31 @@
 namespace RubenSteeb\Testing\RecordList;
 
 use TYPO3\CMS\Recordlist\RecordList\DatabaseRecordList;
+use TYPO3\CMS\Lang\LanguageService;
+
 
 class TestRecordList extends DatabaseRecordList {
 
 
+    /**
+     * @var LanguageService;
+     */
+    protected $languageService;
+
+    /**
+     * @param languageService $languageService
+     */
+    public function injectLanguageService(LanguageService $languageService) {
+        $this->languageService = $languageService;
+    }
+
     public function __construct() {
         parent::__construct();
-        $this->getLanguageService()->includeLLFile('EXT:backend/Resources/Private/Language/locallang_layout.xlf');
+        
     }
 
     public function getTable($tableName, $id, $fields ='') {
-        $warningText = $this->getLanguageService()->getLL('DeleteWarning');
+        $warningText = $this->languageService->includeLLFile('EXT:backend/Resources/Private/Language/locallang_layout.xlf');
         \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($warningText, 'Delete Warning Text');
         return 'Output Of the Record List';
     }
