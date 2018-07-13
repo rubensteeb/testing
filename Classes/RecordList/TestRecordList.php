a8b7f24c7b2c29f9f1cf359e2a7a5db87fc4f3d4
Ruben Steeb <ruben.steeb@gmail.com>
2018-07-13 10:02:24 +0200

    [TASK] Render Output from withing TestRecordList

--- Classes/RecordList/TestRecordList.php
+++ Classes/RecordList/TestRecordList.php
@@ -4,5 +4,9 @@ namespace RubenSteeb\Testing\RecordList;
 use TYPO3\CMS\RecordList\DatabaseRecordList;
 
 class TestRecordList extends DatabaseRecordList {
-    
+
+
+    public function getTable($tableName, $id, $fields ='') {
+        return 'Output Of the Record List';
+    }
 }
