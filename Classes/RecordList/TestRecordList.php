2210772746894904247e39b270df9d2d2602cffb
Ruben Steeb <ruben.steeb@gmail.com>
2018-07-17 08:22:30 +0200

    [DEBUG]Debug makeControl Function

--- Classes/RecordList/TestRecordList.php
+++ Classes/RecordList/TestRecordList.php
@@ -646,7 +646,7 @@ class TestRecordList extends DatabaseRecordList {
      */
     public function makeControl($table, $row)
     {
-        $module = $this->getModule();
+        
         $rowUid = $row['uid'];
         if (ExtensionManagementUtility::isLoaded('version') && isset($row['_ORIG_uid'])) {
             $rowUid = $row['_ORIG_uid'];
