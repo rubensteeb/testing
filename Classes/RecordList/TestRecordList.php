e9da87ec07b19fc94695815a4726a5491f433b66
Ruben Steeb <ruben.steeb@gmail.com>
2018-07-17 08:30:54 +0200

    [DEBUG]Debug makeControl Function

--- Classes/RecordList/TestRecordList.php
+++ Classes/RecordList/TestRecordList.php
@@ -693,7 +693,7 @@ class TestRecordList extends DatabaseRecordList {
         $onClick = 'top.launchView(' . GeneralUtility::quoteJSvalue($table) . ', ' . (int)$row['uid'] . '); return false;';
         $viewBigAction = '<a class="btn btn-default" href="#" onclick="' . htmlspecialchars($onClick) . '" title="' . htmlspecialchars($this->getLanguageService()->getLL('showInfo')) . '">'
             . $this->iconFactory->getIcon('actions-document-info', Icon::SIZE_SMALL)->render() . '</a>';
-        // $this->addActionToCellGroup($cells, $viewBigAction, 'viewBig');
+        $this->addActionToCellGroup($cells, $viewBigAction, 'viewBig');
         // "Move" wizard link for pages/tt_content elements:
         if ($permsEdit && ($table === 'tt_content' || $table === 'pages')) {
             $onClick = 'return jumpExt(' . GeneralUtility::quoteJSvalue(BackendUtility::getModuleUrl('move_element') . '&table=' . $table . '&uid=' . $row['uid']) . ');';
